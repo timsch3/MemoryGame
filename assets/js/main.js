5a3c9455cb56c4da66f10349f61712ac23a28470
timsch3 <timsch3@gmail.com>
2021-08-22 13:42:55 +0200

    changed random images url to hopefully really only give back unique photos now

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let board = document.getElementById('board')
 let cards = []
 let cardsFlipped = 0
+let twoFlipped = false
 let firstCard
 let firstCardOverlay
 let secondCard
@@ -19,21 +20,21 @@ function shuffleCards() {
     // shuffle cards array
     cards.sort((a, b) => 0.5 - Math.random())
     // set random images to card pairs
-    let random = getRandomIntInclusive(1, 9999)
-    cards[0].style.background = cards[1].style.background = 'url(https://picsum.photos/seed/' + random * 1 + '/500) center/cover no-repeat'
-    cards[2].style.background = cards[3].style.background = 'url(https://picsum.photos/seed/' + random * 2 + '/500) center/cover no-repeat'
-    cards[4].style.background = cards[5].style.background = 'url(https://picsum.photos/seed/' + random * 3 + '/500) center/cover no-repeat'
-    cards[6].style.background = cards[7].style.background = 'url(https://picsum.photos/seed/' + random * 4 + '/500) center/cover no-repeat'
-    cards[8].style.background = cards[9].style.background = 'url(https://picsum.photos/seed/' + random * 5 + '/500) center/cover no-repeat'
-    cards[10].style.background = cards[11].style.background = 'url(https://picsum.photos/seed/' + random * 6 + '/500) center/cover no-repeat'
-    cards[12].style.background = cards[13].style.background = 'url(https://picsum.photos/seed/' + random * 7 + '/500) center/cover no-repeat'
-    cards[14].style.background = cards[15].style.background = 'url(https://picsum.photos/seed/' + random * 8 + '/500) center/cover no-repeat'
+    let random = getRandomIntInclusive(1, 99999)
+    cards[0].style.background = cards[1].style.background = 'url(https://picsum.photos/seed/' + (random + 1) + '/500) center/cover no-repeat'
+    cards[2].style.background = cards[3].style.background = 'url(https://picsum.photos/seed/' + (random + 2) + '/500) center/cover no-repeat'
+    cards[4].style.background = cards[5].style.background = 'url(https://picsum.photos/seed/' + (random + 3) + '/500) center/cover no-repeat'
+    cards[6].style.background = cards[7].style.background = 'url(https://picsum.photos/seed/' + (random + 4) + '/500) center/cover no-repeat'
+    cards[8].style.background = cards[9].style.background = 'url(https://picsum.photos/seed/' + (random + 5) + '/500) center/cover no-repeat'
+    cards[10].style.background = cards[11].style.background = 'url(https://picsum.photos/seed/' + (random + 6) + '/500) center/cover no-repeat'
+    cards[12].style.background = cards[13].style.background = 'url(https://picsum.photos/seed/' + (random + 7) + '/500) center/cover no-repeat'
+    cards[14].style.background = cards[15].style.background = 'url(https://picsum.photos/seed/' + (random + 8) + '/500) center/cover no-repeat'
 }
 
 function flipCard(id) {
     let card = document.getElementById(id)
     let cardOverlay = card.firstElementChild
-    if (card != firstCard && card != secondCard) {
+    if (card != firstCard && card != secondCard && !twoFlipped) {
         // flip card and track number of cards already flipped
         card.style.transform = 'rotateY(0)'
         cardOverlay.style.background = 'linear-gradient(to top left, var(--cardBacksideTransp), var(--cardBacksideTransp) 20%, var(--cardBackside2Transp) 20%, var(--cardBackside2Transp) 40%, var(--cardBacksideTransp) 40%, var(--cardBacksideTransp) 60%, var(--cardBackside2Transp) 60%, var(--cardBackside2Transp) 80%, var(--cardBacksideTransp) 80%, var(--cardBacksideTransp) 100%)'
@@ -48,33 +49,35 @@ function flipCard(id) {
                 secondCard = card
                 secondCardOverlay = cardOverlay
                 tries++
+                // check for pairs, make them animate and be shown on top of all other cards
+                if (firstCard.style.background == secondCard.style.background) {
+                    pairsFound++
+                    firstCard.onclick = secondCard.onclick = null
+                    firstCard.style.position = secondCard.style.position = 'relative'
+                    firstCard.style.zIndex = secondCard.style.zIndex = zIndex
+                    firstCard.style.animation = secondCard.style.animation = 'pairFound 0.9s ease-in 0.3s 1 forwards'
+                    zIndex++
+                }
+                // reset flipped cards
+                else if (firstCard.style.background != secondCard.style.background) {
+                    twoFlipped = true
+                    setTimeout(() => {
+                        firstCard.style.transform = secondCard.style.transform = 'rotateY(180deg)'
+                        firstCardOverlay.style.background = secondCardOverlay.style.background = 'linear-gradient(to top left, var(--cardBackside), var(--cardBackside) 20%, var(--cardBackside2) 20%, var(--cardBackside2) 40%, var(--cardBackside) 40%, var(--cardBackside) 60%, var(--cardBackside2) 60%, var(--cardBackside2) 80%, var(--cardBackside) 80%, var(--cardBackside) 100%)'
+                        firstCard.style.transition = secondCard.style.transition = 'all .3s linear 0s'
+                        firstCardOverlay.style.transition = secondCardOverlay.style.transition = 'all .3s linear 0s'
+                        twoFlipped = false
+                        firstCard = secondCard = null
+                    }, 1500)
+                }
+                cardsFlipped = 0
                 break;
         }
-        // check for pairs, make them animate and be shown on top of all other cards
-        if (cardsFlipped == 2 && firstCard.style.background == secondCard.style.background) {
-            pairsFound++
-            firstCard.onclick = secondCard.onclick = null
-            firstCard.style.position = secondCard.style.position = 'relative'
-            firstCard.style.zIndex = secondCard.style.zIndex = zIndex
-            firstCard.style.animation = secondCard.style.animation = 'pairFound 0.9s ease-in 0.3s 1 forwards'
-            zIndex++
-        }
-        // reset flipped cards
-        else if (cardsFlipped == 3) {
-            if (firstCard.style.background != secondCard.style.background) {
-                firstCard.style.transform = secondCard.style.transform = 'rotateY(180deg)'
-                firstCardOverlay.style.background = secondCardOverlay.style.background = 'linear-gradient(to top left, var(--cardBackside), var(--cardBackside) 20%, var(--cardBackside2) 20%, var(--cardBackside2) 40%, var(--cardBackside) 40%, var(--cardBackside) 60%, var(--cardBackside2) 60%, var(--cardBackside2) 80%, var(--cardBackside) 80%, var(--cardBackside) 100%)'
-                firstCard.style.transition = secondCard.style.transition = 'all .3s linear 0s'
-                firstCardOverlay.style.transition = secondCardOverlay.style.transition = 'all .3s linear 0s'
-            }
-            firstCard = card
-            firstCardOverlay = cardOverlay
-            secondCard = secondCardOverlay = null
-            cardsFlipped = 1
-        }
-    } // play animation and output score when finished
+    }
+    // play animation and output score when finished
     if (pairsFound == 8) {
         setTimeout(() => {
+            cards.sort()
             let n = 0
             cards.forEach((c) => {
                 c.style.animation = 'finished ' + (0.4 + n) + 's linear 1 forwards'
